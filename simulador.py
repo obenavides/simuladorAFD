d457e2302d259a7662b44115c2cf5ce4a55327ab
Oscar Benavides <oebenavidesv@gmail.com>
2020-03-03 21:15:37 -0500

    El diccionario se carga segun el contenido del archivo programa.txt.

--- simulador.py
+++ simulador.py
@@ -1,9 +1,13 @@
-d={
-(0,'1'):1,
-(1,'1'):2,
-(2,'1'):3,
-(3,'1'):1
-}
+d={}
+F=set()
+programa=open('programa.txt')
+for linea in programa:
+    q,s,n=linea.split()
+    if '*' in q:
+        q=q.strip('*')
+        F.add(q)
+    d[q,s]=n
+programa.close()
 
 def AFD(d,q0,F,cinta):
     q=q0
@@ -16,5 +20,5 @@ mensaje={True:'Aceptada',False:'Rechazada'}
 cintas=open('cintas.txt')
 for cinta in cintas:
     cinta=cinta.strip()
-    print('La entrada',cinta," es ",mensaje[AFD(d,0,{3},cinta)])
+    print('La entrada',cinta," es ",mensaje[AFD(d,'0',F,cinta)])
 cintas.close()
