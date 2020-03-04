b5758a441ab58f937b519ff0be28710959c40483
Oscar Benavides <oebenavidesv@gmail.com>
2020-03-03 21:14:58 -0500

    La funcion de transicion ahora es un diccionario con duplas como clave.

--- simulador.py
+++ simulador.py
@@ -1,20 +1,20 @@
 d={
-0:{'1':1},
-1:{'1':2},
-2:{'1':3},
-3:{'1':1}
+(0,'1'):1,
+(1,'1'):2,
+(2,'1'):3,
+(3,'1'):1
 }
 
 def AFD(d,q0,F,cinta):
-	q=q0
-	for simbolo in cinta:
-		q=d[q][simbolo]
-	return q in F
+    q=q0
+    for simbolo in cinta:
+        q=d[q,simbolo]
+    return q in F
 
 mensaje={True:'Aceptada',False:'Rechazada'}
 
 cintas=open('cintas.txt')
 for cinta in cintas:
-	cinta=cinta.strip()
-	print('La entrada',cinta," es ",mensaje[AFD(d,0,{3},cinta)])
+    cinta=cinta.strip()
+    print('La entrada',cinta," es ",mensaje[AFD(d,0,{3},cinta)])
 cintas.close()
